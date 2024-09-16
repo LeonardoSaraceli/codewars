885e5250f70c359ad1b0467c8b12c8e59150ed0a
LeonardoSaraceli <leonardolodi09@gmail.com>
2024-09-16 20:50:17 +0100

    Feat: add 6-kyu kata

---
+++ 6-kyu/replace-with-alphabet-position.js
@@ -0,0 +1,60 @@
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// Input = "The sunset sets at twelve o' clock."
+// Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+function alphabetPosition(text) {
+  const alphabet = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  }
+
+  const textArr = text.trim().split('')
+
+  if (!textArr.some((t) => typeof t === 'string')) return
+
+  const alphabetArr = Object.entries(alphabet)
+
+  const res = []
+
+  textArr.forEach((t) => {
+    alphabetArr.forEach((letter) => {
+      if (letter[0] === t.toLowerCase()) {
+        res.push(letter[1])
+      }
+    })
+  })
+
+  return res.join(' ')
+}
